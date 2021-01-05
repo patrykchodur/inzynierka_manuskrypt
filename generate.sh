180c5055f0a2926018011d14e64154f69ba76f96
Patryk Chodur <patrykchodur@icloud.com>
2021-01-05 02:26:37 +0100

    fifth day of writing

--- generate.sh
+++ generate.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+function print_usage {
+	echo "Usage:
+	$0 [-r] file"
+}
+
+function process {
+	pdflatex -halt-on-error "$NAME".tex
+	return $?
+}
+
+if [ $# -lt 1 ]; then
 	echo "Error: please provide .tex name"
+	print_usage
 	exit -1
 fi
 
-NAME=${1%".tex"}
+if [ $1 = "-r" ]; then
+	NAME=${2%".tex"}
+	REPEAT=true
+else
+	NAME=${1%".tex"}
+	REPEAT=false
+fi
 
-pdflatex "$NAME".tex
-pdflatex "$NAME".tex
+
+if ! [ -f "${NAME}.tex" ]; then
+	echo "Error: file does not exist"
+	print_usage
+	exit -1
+fi
+
+process "$NAME".tex
+RETVAL=$?
+if [ $REPEAT = true ]; then
+	process "$NAME".tex
+	RETVAL=$?
+fi
+
+exit $RETVAL
+
